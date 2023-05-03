d03070fe34794d96be26be3409e780ac11b95e9a
UITAK <uik1995@gmail.com>
2023-05-03 23:30:21 +0900

    update

--- src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
@@ -22,5 +22,13 @@ public class DecoratorPatternTest {
 		client.execute();
 	}
 	
+	@Test
+	void decorator2() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		Component timeDecorator = new TimeDecorator(messageDecorator);
+		DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+		client.execute();
+	}
 	
 }
