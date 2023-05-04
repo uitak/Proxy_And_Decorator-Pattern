5935d3d6dabce1baf2f2935be5421c3d5b72cebf
UITAK <uik1995@gmail.com>
2023-05-04 22:41:43 +0900

    update

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteProxyTest.java
@@ -0,0 +1,21 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcreteProxyTest {
+
+	@Test
+	void noProxy() {
+		ConcreteLogic concreateLogic = new ConcreteLogic();
+		ConcreteClient client = new ConcreteClient(concreateLogic);
+		client.execute();
+	}
+	
+	@Test
+	void addProxy() {
+		ConcreteLogic concreteLogic = new ConcreteLogic();
+		TimeProxy timeProxy = new TimeProxy(concreteLogic);
+		ConcreteClient client = new ConcreteClient(timeProxy);
+		client.execute();
+	}
+}
